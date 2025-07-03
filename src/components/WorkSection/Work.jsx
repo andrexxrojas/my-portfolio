d30bee5db9529a8f3d29fc4930728db366932f55
Andre <jayandrerojas@gmail.com>
2025-07-03 19:36:25 +0800

    Added appear animation for work section boxes

--- src/components/WorkSection/Work.jsx
+++ src/components/WorkSection/Work.jsx
@@ -1,9 +1,45 @@
 import styles from "./Work.module.css";
+import { gsap } from "gsap";
+import { ScrollTrigger} from "gsap/ScrollTrigger";
+import { useRef, useEffect } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Work() {
+    const containerRef = useRef(null);
+
+    useEffect(() => {
+        const boxes = gsap.utils.toArray(`.${styles["work-box"]}`)
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: containerRef.current,
+                start: "top 80%",
+                toggleActions: "play none none none",
+                once: true
+            }
+        })
+
+        tl.fromTo(
+            boxes,
+            { opacity: 0, y: 50 },
+            {
+                opacity: 1,
+                y: 0,
+                stagger: 0.2,
+                duration: 1,
+                ease: "power2.out",
+            }
+        )
+
+        return () => {
+            tl.scrollTrigger?.kill()
+        }
+    })
+
     return (
         <div className={styles["wrapper"]}>
-            <div className={styles["work-container"]}>
+            <div className={styles["work-container"]} ref={containerRef}>
                 <div className={styles["work-box"]}></div>
                 <div className={styles["work-box"]}></div>
                 <div className={styles["work-box"]}></div>
